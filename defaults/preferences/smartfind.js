1aa5f48ca3b0d30a58821aa656ab3f19683852f1
tnoleto <tnoleto@53109ca4-e04f-0410-a383-61b69e975762>
2008-06-20 15:35:43 +0000

    Adding branch to get smart find working with findbar

---
+++ defaults/preferences/smartfind.js
@@ -0,0 +1,5 @@
+pref("extensions.smartfind.boolpref", false);
+pref("extensions.smartfind.intpref", 0);
+pref("extensions.smartfind.stringpref", "A string");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.smartfind@smartfind.org.description", "chrome://smartfind/locale/smartfind.properties");
