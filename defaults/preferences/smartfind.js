b715557e30256ffdfabf94d77441a220d418aa75
tnoleto <tnoleto@53109ca4-e04f-0410-a383-61b69e975762>
2008-06-26 14:26:21 +0000

    SmartFind is now enabled by default

--- defaults/preferences/smartfind.js
+++ defaults/preferences/smartfind.js
@@ -1,4 +1,4 @@
-pref("extensions.smartfind.enabled", false);
+pref("extensions.smartfind.enabled", true);
 pref("extensions.smartfind.similarity_level", 50);
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.smartfind@smartfind.org.description", "chrome://smartfind/locale/smartfind.properties");
